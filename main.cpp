fc5bff2b2e74f472e4cb03b43caed5808007c375
hq <hq_cml@163.com>
2020-05-01 16:49:13 +0800

    函数模板

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
+extern void TestTemplateFunction();
+
 int print() {
     cout << "Hello world!" << endl;
     return 0;
 }
 
 int main() {
-
-    print();
+    TestTemplateFunction();
     return 0;
 }
